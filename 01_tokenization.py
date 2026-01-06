fbcaed0259ddabe6fcdbffe8d8129bef3e74f5f3
Aaddityaaa <adityau5090@gmail.com>
2026-01-06 09:12:12 +0530

    Tokenization Process in AI

---
+++ 01_tokenization.py
@@ -0,0 +1,13 @@
+import tiktoken
+
+encoder = tiktoken.encoding_for_model("gpt-4o")
+
+print("Vocab size : ", encoder.n_vocab)
+
+text = "The cat sat on the mat"
+tokens = encoder.encode(text);
+
+print("Tokens : ", tokens)
+# output = [976, 9059, 10139, 402, 290, 2450]
+
+print(encoder.decode([976, 9059, 10139, 402, 290, 2450]))
